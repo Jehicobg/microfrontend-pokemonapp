f9717e3751cf7347ce2b97e62972d77ab4873e39
Jehicobg <66699672+Jehicobg@users.noreply.github.com>
2024-07-13 23:54:04 -0500

    Agregar link dinamicos

--- host/next.config.js
+++ host/next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
+// Importa dotenv para cargar las variables de entorno
+require('dotenv').config();
+
+const app1Url = process.env.APP1_URL || "http://localhost:3001";
+const app2Url = process.env.APP2_URL || "http://localhost:3002";
+const app3Url = process.env.APP3_URL || "http://localhost:3003";
+
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 
 const nextConfig = {
@@ -8,23 +14,19 @@ const nextConfig = {
   },
   webpack: (config, options) => {
     const { isServer } = options;
+
     config.plugins.push(
       new NextFederationPlugin({
         name: "host",
         filename: "static/chunks/remoteEntry.js",
         remotes: {
-          app1: `app1@http://localhost:3001/_next/static/${
-            isServer ? "ssr" : "chunks"
-          }/remoteEntry.js`,
-          app2: `app2@http://localhost:3002/_next/static/${
-            isServer ? "ssr" : "chunks"
-          }/remoteEntry.js`,
-          app3: `app3@http://localhost:3003/_next/static/${
-            isServer ? "ssr" : "chunks"
-          }/remoteEntry.js`,
+          app1: `${app1Url}/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+          app2: `${app2Url}/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+          app3: `${app3Url}/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
         },
       })
     );
+
     return config;
   },
 };
